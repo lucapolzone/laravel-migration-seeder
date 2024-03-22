9ef9166b3ee58d8a0bd68a6fb81cfe0858cbfcab
lucapolzone <lucapolzone@gmail.com>
2024-03-22 13:53:00 +0100

    feat: add TrainsSeeder.php

---
+++ database/seeders/TrainsSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+use App\Models\Train;
+
+class TrainsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        //for loop
+        for ($i = 0; $i < 10; $i++) {
+           $train = new Train(); 
+           $train->company = $faker->text(50);
+          
+           
+
+           $train->save();
+        }
+    }
+}
