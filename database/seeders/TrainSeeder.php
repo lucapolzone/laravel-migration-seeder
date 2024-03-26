21277f7652af7d1e3c419c526c545c103a882db8
lucapolzone <lucapolzone@gmail.com>
2024-03-26 09:33:33 +0100

    feat: print trains data with Faker

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+//use Faker ↓
 use Faker\Generator as Faker;
 
 use App\Models\Train;
@@ -15,54 +16,25 @@ class TrainSeeder extends Seeder
      *
      * @return void
      */
-    // public function run(Faker $faker)
-    public function run()
+    public function run(Faker $faker)
     { 
-
-        $trains_data = [
-          [
-            "company" => "Trenitalia",
-            "departure_station" => "Milano",
-            "arrival_station" => "Roma",
-            "departure_time" => "13:03:00",
-            "arrival_time" => "17:03:00",
-            "train_code" => "3652729",
-            "number_of_carriages" => 7
-          ],
-          [
-            "company" => "Trenitalia",
-            "departure_station" => "Genova",
-            "arrival_station" => "Milano",
-            "departure_time" => "06:03:00",
-            "arrival_time" => "09:03:00",
-            "train_code" => "4652727",
-            "number_of_carriages" => 8
-          ]
-        ];
-
-        foreach ($trains_data as $train_data) {
-
+      for ($i = 0; $i < 10; $i++) {
           //step 1
           $train = new Train();
 
           //step 2
-          // $train->company = $faker->text(50);
-
-          $train->fill($train_data);
-
-          // $train->company = $train_data['company'];
-          // $train->departure_station = $train_data['departure_station'];
-          // $train->arrival_station= $train_data['arrival_station'];
-          // $train->departure_time= $train_data['departure_time'];
-          // $train->arrival_time= $train_data['arrival_time'];
-          // $train->train_code= $train_data['train_code'];
-          // $train->number_of_carriages= $train_data['number_of_carriages'];
+          $train->company = $faker->company();
+          $train->departure_station = $faker->address();
+          $train->arrival_station= $faker->address();
+          $train->departure_time= $faker->time();
+          $train->arrival_time= $faker->time();
+          $train->train_code= $faker->randomDigit();
+          $train->number_of_carriages= $faker->numberBetween(5, 9); //min, max
           // $train->on_time;
           // $train->cancelled;
             
           //step 3
           $train->save();
         }
-				
     }
 }
