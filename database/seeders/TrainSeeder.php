41a3034dabecb56f41359efef12998ede4b0658f
lucapolzone <lucapolzone@gmail.com>
2024-03-26 10:38:54 +0100

    feat: starting to add csv file, var_dump on terminal

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -16,25 +16,28 @@ class TrainSeeder extends Seeder
      *
      * @return void
      */
-    public function run(Faker $faker)
+    public function run()
     { 
-      for ($i = 0; $i < 10; $i++) {
-          //step 1
-          $train = new Train();
+          
+      $file = fopen(__DIR__ . "/../csv/train.csv","r");
+      while (!feof($file)) {
+        var_dump(fgetcsv($file)); //lo vedrai sul terminale facendo php artisan db:seed
+      }
+        //step 1
+        // $train = new Train();
 
-          //step 2
-          $train->company = $faker->company();
-          $train->departure_station = $faker->address();
-          $train->arrival_station= $faker->address();
-          $train->departure_time= $faker->time();
-          $train->arrival_time= $faker->time();
-          $train->train_code= $faker->randomDigit();
-          $train->number_of_carriages= $faker->numberBetween(5, 9); //min, max
-          // $train->on_time;
-          // $train->cancelled;
-            
-          //step 3
-          $train->save();
-        }
+        //step 2
+        // $train->company = $faker->company();
+        // $train->departure_station = $faker->address();
+        // $train->arrival_station= $faker->address();
+        // $train->departure_time= $faker->time();
+        // $train->arrival_time= $faker->time();
+        // $train->train_code= $faker->randomDigit();
+        // $train->number_of_carriages= $faker->numberBetween(5, 9); //min, max
+        // $train->on_time;
+        // $train->cancelled;
+          
+        //step 3
+        // $train->save();
     }
 }
